d9375fc6f51614a99567360b4b3ab0f9c5439866
hadihasanpur <hadihasanpur@gmail.com>
2022-05-16 17:43:30 +0430

    dark mode

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+ // mode: 'jit',
+  content: ['./index.html'],
+  darkMode: 'class', // or 'media' or 'class' 
+  theme: {
+    fontFamily: {
+      vazir: ['Vazir']
+    },
+    extend: {
+      lineHeight: {
+        '0': '0',
+      }
+    },
+  },
+  
+  plugins: [],
+}
